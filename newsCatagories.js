a9728812688e580509dd65cc39613db42b5f0e12
maksud190 <maknep3rdoct@gmail.com>
2022-09-04 01:24:44 +0600

    news displayed

--- newsCatagories.js
+++ newsCatagories.js
@@ -23,29 +23,46 @@ const displayCategories = categories =>{
 function displayNewsById(categoryId){
     fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
     .then(res => res.json())
-    .then(data => displayNews(data));
+    .then(data => displayNews(data.data));
 }
 
 const displayNews = news =>{
     console.log(news);
-    const newsContainer = document.getElementById('news-container')
-    const newsCard = document.createElement('div');
+    const newsContainer = document.getElementById('news-container');
+
+    news.forEach(hotNews =>{
+        console.log(hotNews);
+        const newsCard = document.createElement('div');
     newsCard.classList.add('card')
     newsCard.innerHTML =`
     <div class="row g-0">
               <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="">
+                <img src="${hotNews.thumbnail_url}" class="img-fluid rounded-start" alt="">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  <h5 class="card-title">${hotNews.title}</h5>
+                  <p class="card-text">${hotNews.details.slice(0,250)}</p>
+                  <div class="d-flex justify-content-between">
+                   <div class="d-flex">
+                   <p class="me-3"><img src="${hotNews.author.img}"      class="img-fluid rounded-circle" style="width: 2rem;"></p>
+                  <p class="card-text"><small class="text-muted">${hotNews.author.name}</small></p>
+                  </div>
+                  <div>
+                  <p class="card-text align-items-center"><small class="text-muted">${hotNews.total_view}</small></p>
+                  </div>
+                  <div>
+                  <button type="button" class="btn btn-primary">Detail</button>
+                  </div>
+                  
                 </div>
               </div>
             </div>
     
     `
+    newsContainer.appendChild(newsCard);
+    })
+    
 }
 
 
